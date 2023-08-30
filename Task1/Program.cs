4a04c82406ac1d3fffb1ae5539f9908857e9f0ac
Кошельников Владимир <vkoshelnikov@bk.ru>
2023-08-30 15:50:27 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Task1
+{
+    class Program
+    {
+
+        public delegate void ActionForMeasuring();
+
+        public static void MeasuringSpeed(string message, ActionForMeasuring action)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            action();
+            Console.Write($"{message}: {stopWatch.Elapsed.TotalMilliseconds} мс");
+            Console.WriteLine();
+            stopWatch.Stop();
+        }
+
+        //посимпатичнее
+        static void Main(string[] args) 
+        {
+            string path = @"C:\\skill\\Unit13HW\\Task1\\Book.txt";
+            string text = File.ReadAllText(path);
+            char[] marks = { ' ', '\n', '\r' };
+
+
+            List<string> bookTextList = new List<string>();
+            LinkedList<string> bookTextLinkedList = new LinkedList<string>();
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            string[] words = noPunctuationText.Split(marks);
+            HashSet<string> uniqueWords = new HashSet<string>();
+            foreach (string word in words)
+            {
+                if (word != "")
+                {
+                    bookTextList.Add(word.ToLower());
+                    bookTextLinkedList.AddLast(word.ToLower());
+                }
+            }
+
+            // запускаем новый таймер
+            MeasuringSpeed("Время затраченное на добавление слова в начало списка",() => bookTextList.Insert(0,"СловоВНачалеСписка"));
+            
+            MeasuringSpeed("Время затраченное на добавление слова в начало связанного списка",() => bookTextLinkedList.AddFirst("СловоВНачалеСвязанногоСписка"));
+            
+            MeasuringSpeed("Время затраченное на добавление слова в середину списка", () => bookTextList.Insert(80000, "СловоВСерединеСписка"));
+            
+            LinkedListNode<string> centralWord = bookTextLinkedList.Find("посимпатичнее");
+            MeasuringSpeed("Время затраченное на добавление слова в середину связанного списка", () => bookTextLinkedList.AddAfter(centralWord, "СловоВСерединеСвязанногоСписка"));
+
+            MeasuringSpeed("Время затраченное на добавление слова в конец списка", () => bookTextList.Add("СловоВКонцеСписка"));
+
+            MeasuringSpeed("Время затраченное на добавление слова в конец связанного списка", () => bookTextLinkedList.AddLast("СловоВКонцеСвязанногоСписка"));
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        public class WordCounter
+        {
+            public WordCounter(string value, int number)
+            {
+                Value = value;
+                Number = number;
+            }
+
+            public string Value { get; set; }
+            public int Number { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            // Считываем текстовый файл
+            string path = @"C:\\skill\\Unit13HW\\Task1\\Book.txt";
+            string text = File.ReadAllText(path);
+
+            // Удаляем лишние знаки препинания и разбиваем на слова
+            char[] marks = { ' ', '\n', '\r' };
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            string[] words = noPunctuationText.Split(marks);
+            
+            // Находим все уникальные слова в книге
+            HashSet<string> uniqueWords = new HashSet<string>();
+            foreach (string word in words)
+            {
+                if (word != "")
+                {
+                    // Сводим все низкий регистр
+                    uniqueWords.Add(word.ToLower());
+                }
+            }
+
+            // Проходимся по всей книге находим количество повторений для каждого слова
+            List<WordCounter> topWords = new List<WordCounter>();
+            foreach (string word in uniqueWords)
+            {
+                int num = 0;
+                foreach (string element in words)
+                {
+                    if (element == word)
+                    {
+                        num++;
+                    }
+                }
+                topWords.Add(new WordCounter(word, num));
+            }
+            // Сортируем список по возрастанию 
+            topWords.Sort((x, y) => x.Number.CompareTo(y.Number));
+
+            // Выводим самые частые слова
+            int countWords = topWords.Count;
+            Console.WriteLine("Список самых частовстречающихся слов в книге:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i})\t{topWords[countWords - i].Value}\t-\t{topWords[countWords - i].Number} повторений");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
